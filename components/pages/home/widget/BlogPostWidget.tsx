8e25dad4d8c6e10dc3f567d514f65ff18e1f1bad
Magnus Nilsen <magnussithnilsen@gmail.com>
2023-01-11 03:19:26 -0500

    update spotify widget

--- components/pages/home/widget/BlogPostWidget.tsx
+++ components/pages/home/widget/BlogPostWidget.tsx
@@ -4,10 +4,7 @@ export const BlogPostWidget = () => {
   return (
     <section className="rounded-[12px] border bg-[#1E1E1E] p-4 text-white">
       <h5 className="mb-2 text-xl">A brief introduction about me</h5>
-      <p className="text-xs">
-        A short personal history as it relates to design and development, and
-        how I&apos;ve found value in the cross-section between both disciplines.
-      </p>
+      <p className="text-xs">w.i.p</p>
 
       <button className="mt-4 rounded-lg bg-[#D9D9D9] p-2 text-xs text-black">
         <Link href="/profile">Read more</Link>
--- lib/spotify.api.ts
+++ lib/spotify.api.ts
@@ -2,6 +2,7 @@ import { Buffer } from 'buffer'
 import querystring from 'querystring'
 
 const NOW_PLAYING_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-playing`
+const RECENTLY_PLAYED_ENDPOINT = `https://api.spotify.com/v1/me/player/recently-played`
 const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`
 
 export interface PlayingNowPayload {
@@ -54,6 +55,25 @@ export const getNowPlaying = async (
   })
 }
 
+export const getRecentPlaying = async (
+  client_id: string,
+  client_secret: string,
+  refresh_token: string
+) => {
+  const { access_token } = await getAccessToken(
+    client_id,
+    client_secret,
+    refresh_token
+  )
+
+  return fetch(RECENTLY_PLAYED_ENDPOINT, {
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+    cache: 'no-store',
+  })
+}
+
 export default async function getNowPlayingItem(
   client_id: string,
   client_secret: string,
@@ -61,7 +81,33 @@ export default async function getNowPlayingItem(
 ): Promise<PlayingNowPayload> {
   const response = await getNowPlaying(client_id, client_secret, refresh_token)
   if (response.status === 204 || response.status > 400) {
-    return null
+    const subRes = await getRecentPlaying(
+      client_id,
+      client_secret,
+      refresh_token
+    )
+
+    const songs = await subRes.json()
+
+    const song = songs.items[0]
+
+    console.log('song: ', song.track.album.name)
+
+    const isPlaying = false
+    const title = song.track.name
+    const artist = song.track.artists.map((_artist) => _artist.name).join(', ')
+    const album = song.track.album.name
+    const albumImageUrl = song.track.album.images[0].url
+    const songUrl = song.track.external_urls.spotify
+
+    return {
+      album,
+      albumImageUrl,
+      artist,
+      isPlaying,
+      songUrl,
+      title,
+    }
   }
 
   const song = await response.json()
